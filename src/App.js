efad0dbda90272c9cec785f55d207321cbed85fe
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-23 14:21:01 +0200

    to check

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from "react";
 import Loading from "./components/Loading";
-import Country from "./components/Country";
+// import Country from "./components/Country";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userInput: "",
+      fullPath: "",
       loading: true,
     };
-    this.handleChange = (e) => {
-      this.setState({
-        userInput: e.target.value.trim(),
-      });
-    };
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        loading: false,
-      });
-    }, 2000);
 
+  componentDidMount() {
     fetch("https://restcountries.eu/rest/v2/all")
       .then((res) => res.json())
       .then((data) => init(data))
@@ -39,14 +30,52 @@ export default class App extends React.Component {
         });
       });
     };
+
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+      });
+    }, 2000);
   }
+
+  changeHandler = (e) => {
+    this.setState({
+      userInput: e.target.value.trim(),
+    });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
 
   render() {
     if (this.state.loading) return <Loading />;
     return (
       <React.Fragment>
-        <h1>Country App</h1>
-        {/* <Country data="sendSomething" /> */}
+        <div>
+          <form onSubmit={this.submitHandler}>
+            <input
+              type="text"
+              onChange={this.changeHandler}
+              placeholder="Write a country name"
+            />
+            <button type="submit">Search</button>
+          </form>
+
+          <h3>
+            Country: {`${this.state.countries} `}
+            <img
+              src={this.state.flags}
+              alt={this.state.countries}
+              width="30px"
+            />
+          </h3>
+          <p>Capital: {this.state.capitals}</p>
+          <p>population: {this.state.populations}</p>
+          <p>language: {this.state.languages}</p>
+          <p>currency: {this.state.currencies}</p>
+        </div>
+        {/* <Country data={this.state.props} /> */}
       </React.Fragment>
     );
   }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,16 +1,11 @@
 import React from "react";
-
-export default class App extends React.Component {
+export default class Country extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return (
-      <React.Fragment>
-        <h4>Result here</h4>
-      </React.Fragment>
-    );
+    return <React.Fragment></React.Fragment>;
   }
 }
