57d825c059577ef5e40078ad6b04e1ad76bdeff1
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-24 14:15:29 +0200

    fixing bug

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,15 +1,12 @@
 import React from "react";
 const Country = (props) => {
   const result = props.data.map((item, i) => {
-    console.log(item);
-
     return (
       <React.Fragment key={i}>
         <h3>
-          {item.name}{" "}
+          {item.name}
           <img src={item.flag} alt={item.name} width="30px" key={i} />
         </h3>
-
         <p> Capital: {item.capital}</p>
         <p> Language: {item.languages[0].name}</p>
         <p> Currency: {item.currencies[0].code}</p>
